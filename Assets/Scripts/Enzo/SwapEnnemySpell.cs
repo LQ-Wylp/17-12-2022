b3cf991ed815dd1647808fb64fa2ac00e9dced26
Nutshell01 <92015705+Nutshell01@users.noreply.github.com>
2022-12-18 17:11:07 +0100

    Update SwapEnnemySpell.cs

--- Assets/Scripts/Enzo/SwapEnnemySpell.cs
+++ Assets/Scripts/Enzo/SwapEnnemySpell.cs
@@ -6,5 +6,19 @@ public class SwapEnnemySpell : Spell
 {
     [SerializeField] MonsterManager _monsterManager;
 
-    
+     private void Start()
+    {
+       
+        _monsterManager = GameObject.FindObjectOfType<MonsterManager>();
+    }
+
+    public override void CastEffect()
+    {
+
+        for (int i = 0; i < _monsterManager.MonstersAlive.Count; i++)
+        {
+            _monsterManager.MonstersAlive[i].GetComponent<EnemyMovement>().isSpeed = true;
+        }
+
+    }
 }
