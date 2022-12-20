94ac50a3e63ed9e262e9fa3e0c2677313e1ae941
Nutshell01 <92015705+Nutshell01@users.noreply.github.com>
2022-12-20 22:58:32 +0100

    added prefab + fuck it les tours de mort nique ta race

--- Assets/Scripts/Enzo/SwapEnnemySpell.cs
+++ Assets/Scripts/Enzo/SwapEnnemySpell.cs
@@ -18,20 +18,28 @@ public class SwapEnnemySpell : Spell
         for (int i = _monsterManager.MonstersAlive.Count - 1; i >= 0; i--)
         {
             MonstreAliveInverse.Add(_monsterManager.MonstersAlive[i]);
+
+
         }
 
-        for(int j = 0; j < _monsterManager.MonstersAlive.Count; j++)
+        for (int j = 0; j < _monsterManager.MonstersAlive.Count; j++)
         {
             EnemyMovement EM1 = _monsterManager.MonstersAlive[j].gameObject.GetComponent<EnemyMovement>();
             EnemyMovement EM2 = MonstreAliveInverse[j].gameObject.GetComponent<EnemyMovement>();
+            EnemyMovement EmStock = EM1;
+
+
+            EM2.i = EmStock.i;
+            EM2.NbPointAtteins = EmStock.NbPointAtteins;
+            EM2.percentLerp = EmStock.percentLerp;
+            EM2.DistanceParcourue = EmStock.DistanceParcourue;
+            EM2.RefreshPoint();
 
             EM1.i = EM2.i;
             EM1.NbPointAtteins = EM2.NbPointAtteins;
             EM1.percentLerp = EM2.percentLerp;
             EM1.DistanceParcourue = EM2.DistanceParcourue;
             EM1.RefreshPoint();
-
-
         }
     }
 
